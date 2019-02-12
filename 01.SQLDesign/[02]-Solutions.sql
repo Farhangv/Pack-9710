7f29393a3b86a0c7d5f6c9be56af7f684528ef4d
Farhang Vafa <farhangv@ymail.com>
2019-02-13 00:09:55 +0330

    Update [02]-Solutions.sql

--- 01.SQLDesign/[02]-Solutions.sql
+++ 01.SQLDesign/[02]-Solutions.sql
@@ -25,3 +25,31 @@ GROUP BY l.LocationID, l.Name, c.Name
 ORDER BY 1,2
 GO
 --Q06
+WITH order_data
+AS
+(
+	SELECT 766 'ProductId', 70 'Qty'
+	UNION
+	SELECT 776 'ProductId', 12 'Qty'
+	UNION
+	SELECT 780 'ProductId', 30 'Qty'
+	UNION
+	SELECT 517 'ProductId', 20 'Qty'
+	UNION
+	SELECT 514 'ProductId', 300 'Qty'
+	UNION
+	SELECT 524 'ProductId', 100 'Qty'
+)
+SELECT p.ProductId, p.Name 'Product', SUM(i.Quantity) 'AvailableQty', o.Qty 'OrderQty', DaysToManufacture,  
+CASE
+WHEN SUM(i.Quantity) < o.Qty AND DaysToManufacture > 2 THEN 'No'
+WHEN SUM(i.Quantity) < o.Qty AND DaysToManufacture <= 2 THEN 'Yes'
+WHEN SUM(i.Quantity) > o.Qty THEN 'Yes'
+END 'CanDeliver'
+FROM Production.Product p
+INNER JOIN Production.ProductInventory i
+ON p.ProductID = i.ProductID
+INNER JOIN order_data o
+ON o.ProductId = p.ProductID
+WHERE p.ProductID IN (766,776,780,517,514,524)
+GROUP BY p.ProductId, p.Name, o.Qty, DaysToManufacture
