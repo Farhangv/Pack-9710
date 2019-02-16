19b279e09ea06499dff2ad5933ceb71923dea2d3
Farhang Vafa <farhangv@ymail.com>
2019-02-16 22:37:00 +0330

    971127

---
+++ 02.CSharp/Session11-971127/ConsoleMethodsDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMethodsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To My App");
+            Console.Write("Your Name:");
+            string name = Console.ReadLine();
+            Console.Write("Your Family:");
+            string family = Console.ReadLine();
+
+            Console.WriteLine(
+                    "Hello " + name + " " + family + " How Are You?"
+                );
+            Console.WriteLine("Hello {0} {1}, How Are You?",name,family);
+            Console.WriteLine($"Hello {name} {family}, How Are You?");
+
+            int number = 123456789;
+            Console.WriteLine($"Number Is : {number:x}");
+
+            Console.WriteLine("Goodbye");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 02.CSharp/Session11-971127/FirstCSharpApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+//namespace FirstCSharpApp.MainProgram.FirstPart
+namespace FirstCSharpApp
+{
+    namespace MainProgram
+    {
+        class Program
+        {
+            static void Main()
+            {
+                {
+
+                }
+                //System.Console.WriteLine("Hello");
+                Console                         .WriteLine("Hello");
+                //System.Text.RegularExpressions.Regex
+                /*
+                 * این
+                 * یک 
+                 * توضیحات چند خطی
+                 * است
+                 */
+
+                Sum(10, 20);
+                Console
+                    
+                    .ReadKey();
+            }
+            /// <summary>
+            /// این یک تابع برای جمع بستن دو عدد صحیح است
+            /// </summary>
+            /// <param name="x">عدد اول صحیح</param>
+            /// <param name="y">عدد دوم صحیح</param>
+            /// <returns>جمع دو عدد</returns>
+            public static int Sum(int x, int y)
+            {
+                return x + y;
+            }
+        }
+    }
+}
