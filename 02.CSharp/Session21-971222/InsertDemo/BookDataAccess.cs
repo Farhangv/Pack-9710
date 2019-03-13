852f2eb49b1d5a9fe12b434ceea2fa3d26130c38
Farhang Vafa <farhangv@ymail.com>
2019-03-13 13:42:28 +0330

    971222

---
+++ 02.CSharp/Session21-971222/InsertDemo/BookDataAccess.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace InsertDemo
+{
+    class BookDataAccess
+    {
+        private readonly string connectionString;
+        private SqlConnection connection;
+        public BookDataAccess(string _connectionString)
+        {
+            connectionString = _connectionString;
+        }
+        public void Insert(Book book)
+        {
+            using (this.connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("INSERT INTO Book VALUES(@Title,@Author,@CreatedDate,@ModifiedDate)", connection))
+                {
+                    command.Parameters.Add(new SqlParameter("@Title", SqlDbType.NVarChar, 50));
+                    command.Parameters.Add(new SqlParameter("@Author", SqlDbType.NVarChar, 50));
+                    command.Parameters.Add(new SqlParameter("@CreatedDate", SqlDbType.DateTime));
+                    command.Parameters.Add(new SqlParameter("@ModifiedDate", SqlDbType.DateTime));
+                    command.Parameters["@Title"].Value = book.Title;
+                    command.Parameters["@Author"].Value = book.Author;
+                    command.Parameters["@CreatedDate"].Value = book.CreatedDate;
+                    command.Parameters["@ModifiedDate"].Value = book.ModifiedDate;
+                    
+                    command.Connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<Book> GetAll()
+        {
+            List<Book> books = new List<Book>();
+            using (this.connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("SELECT * FROM Book", this.connection))
+                {
+                    command.Connection.Open();
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var book = new Book()
+                        {
+                            Id = int.Parse(reader["Id"].ToString()),
+                            Title = reader[1].ToString(),
+                            Author = reader["Author"].ToString(),
+                            CreatedDate = (DateTime)reader["CreatedDate"],
+                            ModifiedDate = (DateTime)reader["ModifiedDate"]
+                        };
+                        books.Add(book);
+                    }
+                }
+            }
+            return books;
+        }
+    }
+}
---
+++ 02.CSharp/Session21-971222/InsertDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseHelper.SetupDatabase();
+            BookDataAccess dataAccess = new BookDataAccess(ConfigurationManager.ConnectionStrings["BookStore"].ConnectionString);
+
+            while (true)
+            {
+                Console.Clear();
+                var books = dataAccess.GetAll();
+                foreach (var book in books)
+                {
+                    Console.WriteLine(book);
+                }
+                Console.WriteLine("---------------------");
+
+                Book newBook = new Book();
+                Console.Write("Title: ");
+                newBook.Title = Console.ReadLine();
+                Console.Write("Author: ");
+                newBook.Author = Console.ReadLine();
+                newBook.CreatedDate = DateTime.Now;
+                newBook.ModifiedDate = DateTime.Now;
+                dataAccess.Insert(newBook);
+
+            }
+
+        }
+
+
+    }
+}
