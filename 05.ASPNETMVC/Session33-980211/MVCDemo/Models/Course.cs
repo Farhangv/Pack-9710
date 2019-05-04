7843ee33030ea106d82a5fea0e7ae5053d966137
Farhang Vafa <farhangv@ymail.com>
2019-05-04 08:39:48 +0430

    980211

---
+++ 05.ASPNETMVC/Session33-980211/MVCDemo/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCDemo.Models
+{
+    public class Course
+    {
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public override string ToString()
+        {
+            return $"{Title}({Duration} Hours)";
+        }
+    }
+}
